22e4a94c9b0276548ed7264c1d34701a6d24ff45
alfonsorojasrvz <nanoyv6@gmail.com>
2020-12-11 20:44:23 +0100

    Passing application coroutine scope to database

--- app/src/main/java/codelabs/room_with_a_view/WordsApplication.kt
+++ app/src/main/java/codelabs/room_with_a_view/WordsApplication.kt
@@ -1,9 +1,12 @@
 package codelabs.room_with_a_view
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
 
 class WordsApplication : Application() {
-
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    var scope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this,scope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
