c69091a92d11569c1c314a101a5359d9b1150108
alfonsorojasrvz <nanoyv6@gmail.com>
2020-12-11 15:51:19 +0100

    Add Room database

--- app/src/main/java/codelabs/room_with_a_view/WordRoomDatabase.kt
+++ app/src/main/java/codelabs/room_with_a_view/WordRoomDatabase.kt
@@ -1,9 +1,11 @@
 package codelabs.room_with_a_view
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-
+//The database class for Room must be abstract and extend RoomDatabase
+@Database(entities = [Word::class], version = 1, exportSchema = false)
 public abstract class WordRoomDatabase : RoomDatabase() {
     abstract fun wordDao()
 
