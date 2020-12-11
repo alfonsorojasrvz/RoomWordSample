56cabb6a129a333f7a63540b87b0f15387cf313a
alfonsorojasrvz <nanoyv6@gmail.com>
2020-12-11 15:21:46 +0100

    Implement DAO

--- app/src/main/java/codelabs/room_with_a_view/WordDao.kt
+++ app/src/main/java/codelabs/room_with_a_view/WordDao.kt
@@ -1,7 +1,22 @@
 package codelabs.room_with_a_view
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface WordDao {
+    //Getting all words ordered alphabetically
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    //Inserting a word
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertWord(word: Word)
+
+    //Deleting all words
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
 }
