1195e949485fed174f5fe2118ff0ead8f8e258da
alfonsorojasrvz <nanoyv6@gmail.com>
2020-12-11 19:59:47 +0100

    Fix database error

--- app/src/main/java/codelabs/room_with_a_view/WordRoomDatabase.kt
+++ app/src/main/java/codelabs/room_with_a_view/WordRoomDatabase.kt
@@ -5,16 +5,21 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-//The database class for Room must be abstract and extend RoomDatabase
-@Database(entities = [Word::class], version = 1, exportSchema = false)
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
 public abstract class WordRoomDatabase : RoomDatabase() {
-    abstract fun wordDao()
+
+    abstract fun wordDao(): WordDao
 
     companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
         @Volatile
-        private var INSTANCE : WordRoomDatabase? = null
+        private var INSTANCE: WordRoomDatabase? = null
 
         fun getDatabase(context: Context): WordRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
@@ -22,9 +27,9 @@ public abstract class WordRoomDatabase : RoomDatabase() {
                     "word_database"
                 ).build()
                 INSTANCE = instance
-                return INSTANCE as WordRoomDatabase
+                // return instance
+                instance
             }
-
         }
     }
 }
